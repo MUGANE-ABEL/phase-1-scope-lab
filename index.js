77b9ea51e0748b6c82d17e033a153eb1d6a9689a
Mugane Abel <muganeabel@gmail.com>
2024-04-20 12:33:15 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName(){
+    return customerName.toUpperCase()
+}
+
+function setBestCustomer() {
+    bestCustomer= "not bob"
+    return bestCustomer
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+    return bestCustomer
+}
+ const leastFavoriteCustomer = "Valarie"
+
+ function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "Mcbook"
+    return leastFavoriteCustomer
+ }
